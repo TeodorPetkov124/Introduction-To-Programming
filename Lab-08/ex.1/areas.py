8265271a68faf1004705849b6b3c2915bba42dce
Teodor Petkov <teodor_1200@abv.bg>
2022-12-20 21:18:06 +0200

    commit

--- Lab-08/ex.1/areas.py
+++ Lab-08/ex.1/areas.py
@@ -1,28 +1,29 @@
+from rectangleArea import rectangle
+from triangleArea import triangle
+from suqareArea import square
+from rhombusArea import rhombus
+from trapezoidArea import trapezoid
+
 x = str(input("Enter a figure: "))
 
 if x == "rectangle":
    a = float(input("Enter a: "))
    b = float(input("Enter b: "))
-   from rectangleArea import rectangle
    print("The are of the rectangle is", rectangle(a, b))
 elif x == "triangle":
     a = float(input("Enter a: ")) 
     b = float(input("Enter b: ")) 
-    from triangleArea import triangle
     print("The are of the triangle is", triangle(a,b))
 elif x == "square":
     a = float(input("Enter a: "))
-    from suqareArea import square
     print("The are of the square is", square(a))
 elif x == "rhombus":
     a = float(input("Enter a: ")) 
-    from rhombusArea import rhombus
     print("The are of the rhombus is", rhombus(a))    
 elif x == "trapezoid":
     a = float(input("Enter a: "))
     b = float(input("Enter b: "))
     h = float(input("Enter h: "))
-    from trapezoidArea import trapezoid
     print("The are of the trapezoid is", trapezoid(a, b, h))
 
 
--- Lab-08/ex.2/calculator.py
+++ Lab-08/ex.2/calculator.py
@@ -1,4 +1,4 @@
-import package
+from package import add, minus, multiply, divide
 
 print(f" 1.Add \n 2.Subtract \n 3.Multiply \n 4.Divide")
 x = int(input("Select operation: "))
@@ -6,20 +6,20 @@ x = int(input("Select operation: "))
 if x == 1:
     a = float(input("Enter first num: "))
     b = float(input("Enter second num: "))
-    print("The addition is ", package.add(a,b))
+    print("The addition is ", add(a,b))
 elif x == 2:
     a = float(input("Enter first num: "))
     b = float(input("Enter second num: "))   
-    print("The substraction is ", package.minus(a,b))
+    print("The substraction is ", minus(a,b))
 elif x == 3:
     a = float(input("Enter first num: "))
     b = float(input("Enter second num: "))   
-    print("The multiplication is ", package.multiply(a,b))
+    print("The multiplication is ", multiply(a,b))
 elif x == 4:
     a = float(input("Enter first num: "))
     b = float(input("Enter second num: "))
     from divideModule import divide     
-    print("The division is ", package.divide(a,b))
+    print("The division is ", divide(a,b))
 else:
     print("Incorrect numbers")    
 
